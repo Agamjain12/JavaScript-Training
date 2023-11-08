066e005c3583ebdbebe5fc997a66caeab338b5cb
agam jain <agamj990@gmail.com>
2023-11-08 19:18:02 +0530

    submitted day-2 practiced code

---
+++ practice-day2.js
@@ -0,0 +1,50 @@
+//                Strings and their properties
+
+// slice
+
+console.log("monastraries".slice(3,6));
+
+let v = "jujutsu";
+let w = v.slice(1,3);
+console.log(w);
+
+// indexOf
+
+console.log("bookkeeper".indexOf('ee')); // indexOf property of string lets you search for more than characters
+console.log("tywen".indexOf('e'));
+
+// trim
+
+const agam = "  meow ";
+console.log(agam.trim()); // removes all the leading and trailing spaces
+// it also removes newlines, tabs, and, similar characters from start and end of the string
+console.log(" okay \n ".trim());
+
+// padStart and padEnd
+
+console.log(String(12345).padStart(1, "0")); //padStart and takes the desired length and padding character
+// as arguments.
+
+console.log(String(123).padEnd(4, "1")); // works same as the padStart but it add substrings to the 
+// end of string
+
+
+// You can split a string on every occurrence of another string with
+// split and join it again with join.
+
+let sentence = "leading case of chlymedia";
+let words = sentence.split(",");
+
+console.log(words);
+
+// joining words again with join method
+
+console.log(words.join(". "));
+
+// repeat method
+
+console.log("agam".repeat(3)); // repeat method repeats the given strings and glue them together
+
+// length method
+
+console.log("reference".length); // returns the length of the string.
