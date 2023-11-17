c4a9fe96487e4a1715fc61ddbd0f1e67ae4b5952
agam jain <agamj990@gmail.com>
2023-11-17 19:02:10 +0530

    day 4 practiced code added

---
+++ practice-day4.js
@@ -0,0 +1,199 @@
+//                        Arrays and methods
+
+const arr = ["me", "why", "ain't", "nothing", "but"];
+arr.push("a"); // elements to insert at the end of array
+arr.unshift("tell") // elements to insert at the start
+arr.push("heartbrake");
+
+arr.unshift("tell");
+arr.push("heartbrake");
+
+arr.pop(); // deletes the last element from array
+arr.shift(); // deletes the first element from array
+
+const arr1 = ["tell", "me", "why", "ain't", "nothing", "but", "a", "mistake"];
+
+const arr2 = arr.concat(arr1);
+
+console.log(arr2.includes("tell")); // expected output true
+
+arr2.reverse(); // rotates the array
+
+arr1.slice(1,3); // copies the given portion of the array(3 is exclusive), original array remains unchanged
+arr1.splice(0,1); // removes the specified indexed elements from array, original array is modified
+arr1.splice (2,0,"meow","feline"); // adds elements to specifed index 
+arr1.sort()
+
+
+
+// 2D arrays
+
+let arrd = [
+    ['preticia','morgan','emily'],
+    ['megan','thee','stallion'],
+    ['double','decker','bus']
+]
+
+for(let i =0; i<arrd.length; i++){
+    const names = arrd[i];
+    for(let j=0; j<names.length; j++){
+        console.log(names[j]);
+    }
+}
+
+// or
+
+for(let row of arrd){
+    for(let name of row){
+        console.log(name)
+    }
+}
+
+
+//               for each loop
+
+const saf = [1,2,3,4,5,6,7,8,9];
+
+saf.forEach(function(ele){
+    console.log(ele);
+})
+
+
+
+//                        Maps in array
+
+
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, 
+{first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, 
+{first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, 
+{first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+
+
+const firstNames = fullNames.map(function(names){
+    return names.first; })
+
+
+//   filter method in array
+
+const numbers = [1,2,3,4,5,6];
+const oddNumbers = numbers.filter(number => number%2 != 0);
+
+
+// object creation and accessing the properties
+
+const con = {
+    height: "34cm",
+    chest: 33,
+    neck: "god knows",
+    background: {city: "bleh", country: "winterfell"}
+}
+
+// using a construction function
+
+function Man(skill) {
+     this.skill = skill
+}
+
+const man = new Man("mainsplaining");
+console.log(man);
+
+//accessing the properties
+
+//by dot notation
+
+con.height = "35cm";
+
+// Bracket notation
+
+con["neck"] = "how am i supposed to know that";
+
+// non-inherited properties of objects can be deleted 
+
+delete con.neck;
+
+console.log("neck" in con); //false
+
+// fetching properties of objects using for..in loop
+
+const faceCard = {
+    devis: 'valid',
+    mark: 'invalid',
+    nemoy: 'valid',
+    beck: 'valid'
+}
+
+for(let check in faceCard){
+    console.log(`${check} facecard is ${faceCard[check]}`)
+}
+
+// accessing values only
+
+for(let val in Object.values(faceCard)){
+    console.log(val);
+}
+
+
+
+
+//                           this keyword
+
+const ars = {
+    name : "agam",
+    func : function(){
+        return this.name; // implicit binding
+    }
+}
+
+console.log(ars.func()); // expencted output 'agam'. this when used in the method, refers to the object
+
+//           Explicit binding(when binded with call(), apply(), bind() method)
+
+
+function ageVerify(){ 
+    if(this.age> 18){ 
+        console.log("Yes you can ride the roller coster"); 
+    } else { 
+        console.log("Oopsie you cannot ride the roller coster"); 
+    } 
+} 
+  
+const per1 = {age: 21}; 
+const per2 = {age: 16}; 
+ageVerify.call(per1); 
+ageVerify.call(per2);
+
+
+//                default binding(when this keyword is used in global scope, this is set to window object)
+
+
+
+const age = 22; 
+function verifyAge (){ 
+    return this.age; 
+} 
+  
+console.log(verifyAge()); // expected output undefined
+
+
+
+//                  program to create a function to add key value pair to an object
+
+
+function add(obj, key, value){
+    if(!obj.hasOwnProperty(key)){
+        obj[key] = value;
+        console.log(`added key-value pair ${key} => ${value}`);
+    }
+    else{
+        console.log("this pair already exists");
+    }
+}
+
+const myObj = {
+    name: 'agam',
+    age: 22
+};
+
+add(myObj, "haircolor", "blue");
+console.log(myObj); // expected output {"name": "agam",  "age": 22, "haircolor": "blue"}
